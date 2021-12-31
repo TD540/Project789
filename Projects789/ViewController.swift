467b78647a4f34781b9b4ec721819dd1ac05229d
Thomas Decrick <thomas.decrick@icloud.com>
2021-12-31 17:35:12 +0100

    letter logic

--- Projects789/ViewController.swift
+++ Projects789/ViewController.swift
@@ -8,61 +8,139 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var wordLabel = [UILabel]()
-    var letterView = UIView()
-    var guessAmount = 0 {
+    let letterView = UILabel()
+    let guessesLeft = UILabel()
+    let guessesView = UIView()
+    let guessButton = UIButton(type: .system)
+    let letterInput = UITextField()
+    let incorrectGuessesAllowed = 7
+    
+    var word = String()
+    
+    var guesses = [Character]() {
         didSet {
-            if guessAmount == 7 {
+            if let lastGuess = guesses.last {
+                if word.contains(lastGuess) {
+                    print("last guess = \(lastGuess)")
+                }
+            }
+        }
+    }
+    
+    var incorrectGuessCount = 0 {
+        didSet {
+            if incorrectGuessCount == incorrectGuessesAllowed {
+                incorrectGuessCount = 0
                 newGame()
             }
         }
     }
     
-    func getWord() {
+    @objc func newGame() {
         let url = URL(string: "https://random-word-api.herokuapp.com//word?number=1")!
         if let data = try? Data(contentsOf: url) {
             let decoder = JSONDecoder()
             if let randomWord = try? decoder.decode([String].self, from: data) {
-                if let word = randomWord.first {
+                if let randomWord = randomWord.first {
+                    word = randomWord
                     print("The word is \(word)")
+                    let count = word.count
                     DispatchQueue.main.async { [weak self] in
-                        guard let letterView = self?.letterView else { return }
-                        var prevWidth: CGFloat = 0
-                        for (_, letter) in word.enumerated() {
-                            let letterLabel = UILabel()
-                            letterLabel.text = String(letter)
-                            letterLabel.sizeToFit()
-                            let frame = CGRect(x: prevWidth, y: 0, width: letterLabel.frame.width, height: letterLabel.frame.height)
-                            letterLabel.frame = frame
-                            prevWidth += letterLabel.frame.width
-                            letterView.addSubview(letterLabel)
-                        }
+                        self?.letterView.text = String(repeating: "?", count: count)
                     }
                 }
             }
         }
     }
     
-    @objc func newGame() {
-        guessAmount = 0
-        getWord()
+    /// Make textfield accept only one letter
+    @objc private func letterInputChanged(sender: UITextField) {
+        guard let input = sender.text else { return }
+        switch input.count {
+        case 1:
+            // user adds first input
+            let firstInput = input.lowercased().first!
+            if firstInput.isLetter && !guesses.contains(firstInput) {
+                sender.text = String(firstInput)
+                guessButton.isEnabled = true
+            } else {
+                sender.text = nil
+            }
+        case 2:
+            // user adds second input
+            let firstInput = input.lowercased().first!
+            let secondInput = input.lowercased().last!
+            // check if second input is valid
+            if secondInput.isLetter && !guesses.contains(secondInput) {
+                sender.text = String(secondInput)
+            } else {
+                sender.text = String(firstInput)
+            }
+        default:
+            guessButton.isEnabled = false
+        }
     }
-    
+
     override func loadView() {
         view = UIView()
         view.backgroundColor = .systemBackground
-        letterView.backgroundColor = .red
-//        letterView.translatesAutoresizingMaskIntoConstraints = false
+        letterView.translatesAutoresizingMaskIntoConstraints = false
+        letterView.font = UIFont.systemFont(ofSize: 50)
         view.addSubview(letterView)
-//        NSLayoutConstraint.activate([
-//            letterView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
-//            letterView.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor)
-//        ])
+        guessesLeft.translatesAutoresizingMaskIntoConstraints = false
+        guessesLeft.text = "\(incorrectGuessesAllowed - incorrectGuessCount) guesses left"
+        view.addSubview(guessesLeft)
+        guessesView.translatesAutoresizingMaskIntoConstraints = false
+        guessesView.layer.borderWidth = 2
+        guessesView.layer.borderColor = UIColor.tertiarySystemFill.cgColor
+        guessesView.backgroundColor = .secondarySystemBackground
+        view.addSubview(guessesView)
+        letterInput.addTarget(self, action: #selector(letterInputChanged(sender:)), for: .editingChanged)
+        letterInput.borderStyle = .roundedRect
+        letterInput.becomeFirstResponder()
+        letterInput.textAlignment = .center
+        letterInput.clearButtonMode = .always
+        letterInput.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(letterInput)
+        guessButton.translatesAutoresizingMaskIntoConstraints = false
+        guessButton.isEnabled = false
+        guessButton.setTitle("Guess!", for: .normal)
+        guessButton.addTarget(self, action: #selector(guessButtonTapped), for: .touchUpInside)
+        view.addSubview(guessButton)
+        NSLayoutConstraint.activate([
+            letterView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            letterView.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor),
+            guessesLeft.topAnchor.constraint(equalTo: letterView.bottomAnchor),
+            guessesLeft.centerXAnchor.constraint(equalTo: letterView.centerXAnchor),
+            guessesView.topAnchor.constraint(equalTo: guessesLeft.bottomAnchor, constant: 10),
+            guessesView.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor),
+            guessesView.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor),
+            guessesView.heightAnchor.constraint(equalToConstant: 50),
+            letterInput.topAnchor.constraint(equalTo: guessesView.bottomAnchor, constant: 10),
+            letterInput.centerXAnchor.constraint(equalTo: guessesView.centerXAnchor),
+            letterInput.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor),
+            guessButton.topAnchor.constraint(equalTo: letterInput.bottomAnchor, constant: 10),
+            guessButton.centerXAnchor.constraint(equalTo: letterInput.centerXAnchor)
+        ])
     }
-
+    
+    @objc func guessButtonTapped(_ sender: UIButton) {
+        if let letter = letterInput.text {
+            if letter.isEmpty == false {
+                let guess = Character(letter)
+                if guesses.contains(guess) == false {
+                    print("Guessing \(guess)")
+                    guesses.append(guess)
+                }
+                letterInput.text = nil
+                guessButton.isEnabled = false
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         performSelector(inBackground: #selector(newGame), with: nil)
     }
-
+    
 }
